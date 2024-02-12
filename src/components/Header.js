4e03f463e54583e34d5d0afb863766313a3b99c2
heymitali <mitalisinghnaruka@gmail.com>
2024-02-12 18:10:28 +0530

    implemented dummy "login" button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import { LOGO } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+  let [logBtn, setLogBtn] = useState("login");
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +15,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="log-btn"
+            onClick={() => {
+              logBtn === "login" ? setLogBtn("logout") : setLogBtn("login");
+            }}
+          >
+            {logBtn}
+          </button>
         </ul>
       </div>
     </div>
